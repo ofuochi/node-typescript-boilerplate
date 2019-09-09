843978014e4ce99c8d28f621fef6836a21437114
fortune <fortuneochi@gmail.com>
2019-09-09 22:58:05 +0100

    Setup mocha test

--- test/controllers/auth.controller.test.ts
+++ test/controllers/auth.controller.test.ts
@@ -1,6 +1,25 @@
+import "mocha";
+import { expect } from "chai";
+
+import UserEntity from "../../src/models/entities/UserEntity";
+import UserRepo from "../../src/db/repos/user.repo";
+
 describe("Hello here", () => {
-	console.log("Hello");
+    const user = UserEntity.createInstance({
+        firstName: "FirstName",
+        lastName: "LastName",
+        email: "email@gmail.com",
+        password: "password",
+        username: "username",
+        tenantId: "tenantId"
+    });
+    user.setAsDeleted();
+    console.log(user);
+    const repo = UserRepo;
+    console.log(repo);
+    before("Running Before", () => {
+        console.log(user);
+
+        expect(1 + 2).to.equal(3);
+    });
 });
-before("Running Before", () => {
-    console.log("Running before")
-})
