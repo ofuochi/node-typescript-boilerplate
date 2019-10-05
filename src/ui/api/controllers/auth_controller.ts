ba6f0868dfb1069b8fbaa8b5b689c2eaeb836c27
fortune <fortuneochi@gmail.com>
2019-10-05 21:56:10 +0100

    Update pre-push hook

--- src/ui/api/controllers/auth_controller.ts
+++ src/ui/api/controllers/auth_controller.ts
@@ -33,6 +33,6 @@ export default class AuthController extends BaseController {
 
     @httpPost("/signIn")
     public async getById(@requestBody() input: UserSignInInput) {
-        return await this._authService.signIn(input);
+        return this._authService.signIn(input);
     }
 }
