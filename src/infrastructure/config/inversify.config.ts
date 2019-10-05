d88b30c9d9b524bc79b0a959e5056b7aa9675f91
Sudarsan Balaji <sudarsanb@sahajsoft.com>
2019-10-05 22:02:38 +0530

    Configure automapper and inject

--- src/infrastructure/config/inversify.config.ts
+++ src/infrastructure/config/inversify.config.ts
@@ -9,6 +9,7 @@ import {
 } from "../../domain/interfaces/repositories";
 import { ILoggerService, IMailService } from "../../domain/interfaces/services";
 import { IAuthService } from "../../ui/interfaces/auth_service";
+import { ITenantService } from "../../ui/interfaces/tenant_service";
 
 // Service implementations
 import LoggerService from "../../domain/services/logger_service";
@@ -23,18 +24,25 @@ import { TenantRepository } from "../db/repositories/tenant_repository";
 import "../../ui/api/controllers/auth_controller";
 import "../../ui/api/controllers/secure_controller";
 import "../../ui/api/controllers/tenant_controller";
+import TenantService from "ui/services/tenant_service";
+import { AutoMapper, Mapper } from 'automapper-nartc';
+import { TenantProfile } from "ui/profiles/tenant_profile";
 
 export const referenceDataIoCModule = new ContainerModule(bind => {
     // Repositories
     bind<ITenantRepository>(TYPES.TenantRepository).to(TenantRepository);
-
     bind<IUserRepository>(TYPES.UserRepository).to(UserRepository);
+
+    // Type Mappings
+    Mapper.initialize(config => {
+        config.addProfile(new TenantProfile());
+    });
+    bind<AutoMapper>(TYPES.AutoMapper).toConstantValue(Mapper);
 
     // Services
     bind<IMailService>(TYPES.MailService).to(MailService);
-
     bind<IAuthService>(TYPES.AuthService).to(AuthService);
-
+    bind<ITenantService>(TYPES.TenantService).to(TenantService);
     bind<ILoggerService>(TYPES.LoggerService).to(LoggerService);
 
     bind<EventDispatcher>(TYPES.EventDispatcher).toConstantValue(
