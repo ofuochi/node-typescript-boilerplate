d0b145d5adde770323ec319247f3f5ed775642d7
Sunrise Ezekikwu <ezesunrise@yahoo.com>
2019-10-22 22:07:12 +0100

    documented base model

--- src/domain/model/base.ts
+++ src/domain/model/base.ts
@@ -8,6 +8,14 @@ import { Writable } from "../utils/writable";
 import { User } from "./user";
 
 // eslint-disable-next-line
+/**
+ * Base entity from which other entities inherit
+ *
+ * @export
+ * @abstract
+ * @class BaseEntity
+ * @extends {Typegoose}
+ */
 @pre<BaseEntity>("findOneAndUpdate", function(this: Query<BaseEntity>, next) {
     try {
         const entity = this.getUpdate();
@@ -47,49 +55,123 @@ export abstract class BaseEntity extends Typegoose {
     abstract type: string;
     abstract update(entity: Partial<BaseEntity>): void;
 
+    /**
+     * Primary key
+     *
+     * @type {*}
+     * @memberof BaseEntity
+     */
     @Expose()
     id?: any;
 
+    /**
+     * Specifies the date and time the entity was created
+     *
+     * @type {Date}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ required: true, default: new Date() })
     readonly createdAt: Date = new Date();
+    /**
+     * This references the user who added this entity
+     *
+     * @type {(Ref<User | null>)}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ default: null, ref: BaseEntity })
     readonly createdBy: Ref<User | null> = null;
+    /**
+     * Secifies the date and time the entity was last updated
+     *
+     * @type {Date}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ default: null })
     readonly updatedAt?: Date;
+    /**
+     * This references the user who performed the last update on this entity
+     *
+     * @type {(Ref<User | null>)}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ default: null, ref: BaseEntity })
     readonly updatedBy: Ref<User | null> = null;
+    /**
+     * Specifies if the entity can be used in the application
+     *
+     * @type {boolean}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ required: true, default: true })
     readonly isActive: boolean = true;
+    /**
+     * Specifies if the entity has been soft-deleted
+     *
+     * @type {boolean}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ required: true, default: false })
     readonly isDeleted: boolean = false;
+    /**
+     * This references the user who deleted this entity if the entity has been soft-deleted
+     *
+     * @type {(Ref<User | null>)}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ default: null, ref: BaseEntity })
     readonly deletedBy: Ref<User | null> = null;
+    /**
+     * The time the entity was deleted if it has been deleted
+     *
+     * @type {Date}
+     * @memberof BaseEntity
+     */
     @Expose()
     @prop({ default: null })
     readonly deletionTime?: Date;
 
+    /**
+     * sets {isDeleted} to true
+     *
+     * @memberof BaseEntity
+     */
     @instanceMethod
     delete(): void {
         (this as Writable<BaseEntity>).isDeleted = true;
     }
 
+    /**
+     * sets {isDeleted} to false
+     *
+     * @memberof BaseEntity
+     */
     @instanceMethod
     restore(): void {
         (this as Writable<BaseEntity>).isDeleted = false;
     }
 
+    /**
+     * sets {isActive} to true
+     *
+     * @memberof BaseEntity
+     */
     @instanceMethod
     deactivate(): void {
         (this as Writable<BaseEntity>).isActive = false;
     }
 
+    /**
+     * sets isActive to false
+     *
+     * @memberof BaseEntity
+     */
     @instanceMethod
     activate(): void {
         (this as Writable<BaseEntity>).isActive = true;
